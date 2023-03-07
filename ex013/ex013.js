d88479652e8b52d2afd24626faee7cb4dbcf4fea
Rub1nh0 <115159926+Rub1nh0@users.noreply.github.com>
2023-03-07 00:09:02 -0300

    exercitando JavaScript

---
+++ ex013/ex013.js
@@ -0,0 +1,28 @@
+var agora = new Date()
+var diaSem = agora.getDay()
+switch(diaSem){
+    case 0:
+        console.log('DOMINGO')
+        break
+    case 0:
+    console.log('DOMINGO')
+    break
+    case 0:
+    console.log('SEGUNDA')
+    break
+    case 0:
+    console.log('TERÇA')
+    break
+    case 0:
+    console.log('QUARTA')
+    break
+    case 0:
+    console.log('QUINTA')
+    break
+    case 0:
+    console.log('SEXTA')
+    break
+    case 0:
+    console.log('SABADO')
+    break
+}
